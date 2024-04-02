c2f1a433e1764720fc3b5ee411ab304fc92b5ebb
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-04-02 11:13:47 +0900

    feat : 카메라 스테킹 및 슬로우모션 FOV조정 기능 추가

--- Assets/Scripts/FPS Camera Controller.cs
+++ Assets/Scripts/FPS Camera Controller.cs
@@ -1,16 +1,40 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FPSCameraController : MonoBehaviour
 {
+    [Header("GameObject")]
     [SerializeField] Transform cameraRoot;
+    [SerializeField] Camera handcam;
     [SerializeField] float mouseSensitivity;
+
+    [Header("Spec")]
+    public float lerpSpeed = 1f;        // Lerp 속도
+    public float InitFOV = 60;
+    public float targetFOV = 60;        // 목표 FOV
+    private float SlowmotionFOV;
+    private bool Zoom;
 
     private Vector2 inputDir;
     private float yRotation;
     private float xRotation;
+
+    private void Start()
+    {
+        cameraRoot.gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = InitFOV;
+        handcam.fieldOfView = InitFOV;
+        SlowmotionFOV = InitFOV;
+        Zoom = false;
+    }
+
+    private void FixedUpdate()
+    {
+        slowmotionZoom();
+    }
 
     private void OnEnable()
     {
@@ -31,8 +55,39 @@ public class FPSCameraController : MonoBehaviour
         cameraRoot.localRotation = Quaternion.Euler(xRotation, 0, 0);
     }
 
+    private void slowmotionZoom()
+    {
+        if (Zoom)
+        {
+            if (SlowmotionFOV != targetFOV)
+            {
+                SlowmotionFOV = Mathf.Lerp(SlowmotionFOV, targetFOV, lerpSpeed * Time.fixedDeltaTime);
+                cameraRoot.gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = SlowmotionFOV;
+                handcam.fieldOfView = SlowmotionFOV;
+            }
+        }
+        else
+        {
+            if (SlowmotionFOV != InitFOV)
+            {
+                SlowmotionFOV = Mathf.Lerp(SlowmotionFOV, InitFOV, lerpSpeed * Time.fixedDeltaTime);
+                cameraRoot.gameObject.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView = SlowmotionFOV;
+                handcam.fieldOfView = SlowmotionFOV;
+            }
+        }
+    }
+
     private void OnLook(InputValue value)
     {
         inputDir = value.Get<Vector2>();
+    }
+
+    private void OnZoom(InputValue value)
+    {
+        if (value.isPressed)
+            Zoom = true;
+        else
+            Zoom = false;
+        
     }
 }
---
+++ Assets/Scripts/Player TakeDamage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTakeDamage : IDamagable
+{
+    public void TakeDamage(int damage)
+    {
+        
+    }
+
+    public void TakeDamage(int damage, Vector3 EnemyPosition)
+    {
+        
+    }
+}
