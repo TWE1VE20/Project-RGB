76d6ecf26e98c9b4b0d04eb4abfd16e6d89f391b
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-04-03 15:52:01 +0900

    refactor : 무기 시스템 켑슐화

---
+++ Assets/Scripts/Weapons/Ammo System.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AmmoSystem: MonoBehaviour
+{
+    public Animator animator;
+    public AudioSource audioSource;
+
+    public float reloadTime;
+    public int AmmoLeft;    // ³²Àº ÃÑ¾Ë °¹¼ö
+    public int maxRounds;   // ÅºÃ¢ ÃÖ´ë Å©±â
+    public int rounds { get; private set; }     // ÇöÁ¦ ÃÑ¿¡ µé¾îÀÖ´Â ÃÑ¾Ë °¹¼ö
+
+    private void Start()
+    {
+        this.rounds = maxRounds;
+    }
+
+    public void Reload()
+    {
+        if (AmmoLeft >= maxRounds)
+        {
+            AmmoLeft -= maxRounds - rounds;
+            rounds = maxRounds;
+            Debug.Log($"Reload {rounds} Rounds");
+            return;
+        }
+        else if (AmmoLeft > 0)
+        {
+            if (rounds == 0)
+            {
+                rounds = AmmoLeft;
+                AmmoLeft = 0;
+            }
+            else
+            {
+                if (AmmoLeft > maxRounds - rounds)
+                {
+                    AmmoLeft -= maxRounds - rounds;
+                    rounds = maxRounds;
+                }
+                else
+                {
+                    rounds += AmmoLeft;
+                    AmmoLeft = 0;
+                }
+            }
+            Debug.Log($"Reload {rounds} Rounds");
+            return;
+        }
+    }
+
+    public bool CanReload()
+    {
+        if (AmmoLeft > 0)
+            return true;
+        else
+        {
+            Debug.Log("No More Ammo Left");
+            return false;
+        }
+    }
+
+    public bool GunAttack()
+    {
+        if (rounds > 0)
+        {
+            rounds--;
+            // Shoot Animation and Sound
+            return true;
+        }
+        else
+        {
+            // no rounds Animation and Sounds
+            return false;
+        }
+    }
+}
--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -6,88 +6,36 @@ public class Gun : Weapons
 {
     [Header("Component")]
     [SerializeField] Animator animator;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AmmoSystem ammoSystem;
 
     [Header("Gun Status")]
     [SerializeField] float timeforReload;
     [SerializeField] float timeforAttack;
-    public int AmmoLeft;        // ³²Àº ÃÑ¾Ë °¹¼ö
+    [SerializeField] int initAmmoAmount;
     public int maxRounds;   // ÅºÃ¢ ÃÖ´ë Å©±â
-    public int rounds { get; private set; }     // ÇöÁ¦ ÃÑ¿¡ µé¾îÀÖ´Â ÃÑ¾Ë °¹¼ö
 
-    public Gun() : base()
-    {
-        this.attackType = Weapons.AttackType.GUN;
-        this.rounds = maxRounds;
-        SetReloadTime(timeforReload);
-        SetAttackTime(timeforAttack);
-    }
-
-    private void Start()
+    private void Awake()
     {
         base.Starting();
-        this.attackType = Weapons.AttackType.GUN;
-        this.rounds = maxRounds;
-        SetReloadTime(timeforReload);
         SetAttackTime(timeforAttack);
+
+        this.attackType = Weapons.AttackType.GUN;
+        if (ammoSystem == null)
+        {
+            ammoSystem = gameObject.AddComponent<AmmoSystem>();
+        }
+        ammoSystem.animator = this.animator;
+        ammoSystem.audioSource = this.audioSource;
+        ammoSystem.reloadTime = this.timeforReload;
+        ammoSystem.AmmoLeft = this.initAmmoAmount;
+        ammoSystem.maxRounds = this.maxRounds;
     }
 
     public override bool Attack()
     {
-        if(rounds > 0) 
-        {
-            rounds--;
-            // Shoot Animation and Sound
-            return true;
-        }
-        else
-        {
-            // no rounds Animation and Sounds
-            return false;
-        }
+        return ammoSystem.GunAttack();
     }
 
-    public override void Reload() 
-    {
-        if(AmmoLeft >= maxRounds)
-        {
-            AmmoLeft -= maxRounds - rounds;
-            rounds = maxRounds;
-            Debug.Log($"Reload {rounds} Rounds");
-            return;
-        }
-        else if(AmmoLeft > 0)
-        {
-            if (rounds == 0)
-            {
-                rounds = AmmoLeft;
-                AmmoLeft = 0;
-            }
-            else
-            {
-                if (AmmoLeft > maxRounds - rounds)
-                {
-                    AmmoLeft -= maxRounds - rounds;
-                    rounds = maxRounds;
-                }
-                else
-                {
-                    rounds += AmmoLeft;
-                    AmmoLeft = 0;
-                }
-            }
-            Debug.Log($"Reload {rounds} Rounds");
-            return;
-        }
-    }
 
-    public override bool CanReload()
-    {
-        if (AmmoLeft > 0)
-            return true;
-        else
-        {
-            Debug.Log("No More Ammo Left");
-            return false;
-        }
-    }
 }
