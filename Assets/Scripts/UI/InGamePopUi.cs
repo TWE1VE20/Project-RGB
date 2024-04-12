cec415ac045635c1d75a6b33c109757dbfbaea3b
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-04-12 16:13:57 +0900

    feat : 팝업 UI 삭제기능 추가

--- Assets/Scripts/UI/InGamePopUi.cs
+++ Assets/Scripts/UI/InGamePopUi.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InGamePopUi : MonoBehaviour
 {
-    
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;       
+    }
+
+    public void Close()
+    {
+        Debug.Log("Close UI");
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        Destroy(gameObject);
+    }
 }
